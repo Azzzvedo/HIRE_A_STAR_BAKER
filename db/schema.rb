0451077f261b40d9dfa2f173243733495082b639
Joshua Walker <josh.wm.walker@gmail.com>
2022-02-22 10:27:52 +0000

    Start of day 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_174115) do
+ActiveRecord::Schema.define(version: 2022_02_21_174421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.boolean "paid"
+    t.bigint "user_id", null: false
+    t.bigint "cake_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cake_id"], name: "index_bookings_on_cake_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cakes", force: :cascade do |t|
     t.string "name"
@@ -37,5 +49,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_174115) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cakes"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cakes", "users"
 end
