52e25f59e8bb0d5519eb68d376270b23e3bf3653
Joshua Walker <josh.wm.walker@gmail.com>
2022-02-22 17:39:55 +0000

    Seed built

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'raker'
+require 'open-uri'
+
+50.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123456',
+    address: Faker::Address.street_address,
+    baker: Faker::Boolean.boolean(true_ratio: 0.2)
+  )
+
+  if user.baker?
+    5.times do
+      cake = Cake.create!(
+        name: Faker::HarryPotter.spell,
+        price: rand(3..16),
+        user: user
+      )
+      file = URI.open('https://source.unsplash.com/random')
+      cake.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+    end
+  end
+end
