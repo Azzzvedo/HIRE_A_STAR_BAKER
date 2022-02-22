e157f3dd34e6824b93aac8f6b9f0939a6b058529
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-02-22 16:52:27 +0000

    bookings new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,31 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show]
   def index
-    @user = current_user
-    @bookings = Booking.where(user: @user)
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def show; end
+
+  def new
+    @cake = Cake.find(params[:cake_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.cake = Cake.find(params[:cake_id])
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:time)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
