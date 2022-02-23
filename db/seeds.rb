a2804582df8dceac11fcc69a60962abcbc0db546
Joshua Walker <josh.wm.walker@gmail.com>
2022-02-23 12:10:25 +0000

    Luz name sorted hopefully

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 User.destroy_all
 
-users = ['arrabella.stephenson@gmail.com', 'ian.wiggins@gmail.com', 'josh.walker@gmail.com', 'luz.valdevinos@gmail.com']
+users = ['arrabella.stephenson@gmail.com', 'ian.wiggins@gmail.com', 'josh.walker@gmail.com', 'luz.valdovinos@gmail.com']
 users.each do |user|
   name = user.split("@")[0].split(".")
   admin = User.create(first_name: name[0].capitalize, last_name: name[1].capitalize, email: user, password: "password", address: Faker::Address.street_address, baker: true)
